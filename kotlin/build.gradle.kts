0234b3ab487437f29cde397219cfd968ffac3c94
TakuFrankM <mukarakatetf@gmail.com>
2026-02-03 10:55:28 +1100

    Kotlin gradle files

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "com.playground.reup"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
