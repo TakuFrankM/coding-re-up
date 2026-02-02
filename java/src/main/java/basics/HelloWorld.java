49ea19eb743d4060fe3afd64ba84388ef774dd19
TakuFrankM <mukarakatetf@gmail.com>
2026-02-03 09:57:19 +1100

    Hello World Java - Simple traditional hello world output

---
+++ java/src/main/java/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+/**
+ * Standard hello world system output (Tradition!)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World :)");
+    }
+}
